6f16902a4f25ef5074185362156bfd9a891ec91e
mik <mik@mik>
2016-02-20 04:15:38 +0300

    added independed loading

--- detectors_test.go
+++ detectors_test.go
@@ -31,23 +31,23 @@ func train_lang_detector() {
 func TestLangDetect(t *testing.T) {
 	// os.RemoveAll("./bases/langs.gob")
 	// train_lang_detector()
-  var lang_samples = map[string]string{
-	"English":   `This is a list of file formats used by computers, organized by type. Filename extensions are usually noted in parentheses if they differ from the format n`,
-	"Spanish":   `El colapso de la Unión Europea desde el interior es iniciada por el gobierno ruso, - así dijo el actual presidente del Parlamento Europeo, Martin Schulz, expresó que apoyar al Presidente de Ucrania sobre la conducta de varios años de guerra de la información en el territorio de los Estados europeos.`,
-	"Russian":   `Что ещё почитать? Twitter Политоты — российские и не только оппозиционные новости, оперативно и полноценно`,
-	"Japanese":  `他に何を読むには？ TwitterのPolitoty - ロシアの迅速かつ完全にだけでなく、野党のニュース`,
-	"Norwegian": `Hvad andet at læse? Twitter Politoty - russisk og ikke kun oppositionen nyheder hurtigt og fuldt ud`,
-	"Bulgarian": `Какво друго да се чете? Twitter Politoty - руски и не само опозицията новини своевременно и напълно`,
-	"Italian":   `Che altro da leggere? Twitter Politoty - russo e non solo notizie opposizione tempestivamente e completamente`,
-	"Hebrew":    `טוויטר Politoty - רוסית ולא חדשות האופוזיציה רק מיד ובאופן מלא`,
-  }
-  
- 	detector, err := LangDetect("./langs.gob")
+	var lang_samples = map[string]string{
+		"English":   `This is a list of file formats used by computers, organized by type. Filename extensions are usually noted in parentheses if they differ from the format n`,
+		"Spanish":   `El colapso de la Unión Europea desde el interior es iniciada por el gobierno ruso, - así dijo el actual presidente del Parlamento Europeo, Martin Schulz, expresó que apoyar al Presidente de Ucrania sobre la conducta de varios años de guerra de la información en el territorio de los Estados europeos.`,
+		"Russian":   `Что ещё почитать? Twitter Политоты — российские и не только оппозиционные новости, оперативно и полноценно`,
+		"Japanese":  `他に何を読むには？ TwitterのPolitoty - ロシアの迅速かつ完全にだけでなく、野党のニュース`,
+		"Norwegian": `Hvad andet at læse? Twitter Politoty - russisk og ikke kun oppositionen nyheder hurtigt og fuldt ud`,
+		"Bulgarian": `Какво друго да се чете? Twitter Politoty - руски и не само опозицията новини своевременно и напълно`,
+		"Italian":   `Che altro da leggere? Twitter Politoty - russo e non solo notizie opposizione tempestivamente e completamente`,
+		"Hebrew":    `טוויטר Politoty - רוסית ולא חדשות האופוזיציה רק מיד ובאופן מלא`,
+	}
+
+	detector, err := LangDetect()
 	if err != nil {
 		t.Error(err)
 	}
 	for k, v := range lang_samples {
 		assert.Equal(t, detector(v).Name, k)
 	}
-  // PASS
+	// PASS
 }
--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package LangDetect
+
 import (
+	"github.com/GeertJohan/go.rice"
 	"github.com/MichaelLeachim/NumBayes"
-	"io/ioutil"
 	"log"
 	"regexp"
 )
+
 var LANGSPLITTER = regexp.MustCompile("[^\\s\\n]+")
 
-func LangDetect(basepath string) (func(data string) LangDetectorLangItem, error) {
-	data, err := ioutil.ReadFile(basepath)
+func LangDetect() (func(data string) LangDetectorLangItem, error) {
+	templateBox, err := rice.FindBox("bases")
 	if err != nil {
 		return nil, err
 	}
+
+	data, err := templateBox.Bytes("langs.gob")
+	if err != nil {
+		return nil, err
+	}
+
 	solver := bayes.BayesMemory{}
 	solver.UnMarshal(data)
 	return func(data string) LangDetectorLangItem {
